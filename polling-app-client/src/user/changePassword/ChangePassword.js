7cea99b8453cb625f22135af66d55fb746eb96e3
Nikola Brodar <brodarnikola@gmail.com>
2024-08-06 16:20:09 +0200

    Now password change works like a charm

--- polling-app-client/src/user/changePassword/ChangePassword.js
+++ polling-app-client/src/user/changePassword/ChangePassword.js
@@ -1,59 +1,45 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
-import {ACCESS_TOKEN, PASSWORD_MAX_LENGTH, PASSWORD_MIN_LENGTH, PASSWORD_DOES_NOT_MATCH} from '../../constants';
-
+import React, { useState, useEffect } from 'react';
+import { Link, useHistory, useLocation } from 'react-router-dom';
+import { ACCESS_TOKEN, PASSWORD_MAX_LENGTH, PASSWORD_MIN_LENGTH, PASSWORD_DOES_NOT_MATCH } from '../../constants';
 import { Form, Input, Button, notification } from 'antd';
-import {showChangePasswordPage, savePassword} from "../../util/APIUtils";
-const FormItem = Form.Item;
+import { showChangePasswordPage, savePassword } from "../../util/APIUtils";
 
-class ChangePassword extends Component {
+const { Item: FormItem } = Form;
 
-    constructor(props) {
-        super(props);
+const ChangePassword = () => {
+    const [password1, setPassword1] = useState({ value: '', validateStatus: '', errorMsg: '' });
+    const [password2, setPassword2] = useState({ value: '', validateStatus: '', errorMsg: '' });
+    const history = useHistory();
+    const location = useLocation();
 
-        this.state = {
-            password1: {
-                value: ''
-            },
-            password2: {
-                value: ''
-            }
+    useEffect(() => {
+        localStorage.setItem(ACCESS_TOKEN, "");
+    }, []);
+
+    const handleInputChange = (event, validationFun) => {
+        const { name, value } = event.target;
+
+        if (name === 'password1') {
+            setPassword1({ value, ...validationFun(value) });
+        } else if (name === 'password2') {
+            setPassword2({ value, ...validationFun(value) });
         }
-        this.handleInputChange = this.handleInputChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.isFormInvalid = this.isFormInvalid.bind(this);
-    }
+    };
 
-    handleInputChange(event, validationFun) {
-        const target = event.target;
-        const inputName = target.name;
-        const inputValue = target.value;
-
-        this.setState({
-            [inputName]: {
-                value: inputValue,
-                ...validationFun(inputValue)
-            }
-        });
-    }
-
-    handleSubmit(event) {
+    const handleSubmit = (event) => {
         event.preventDefault();
 
-        const query = new URLSearchParams(this.props.location.search);
-
+        const query = new URLSearchParams(location.search);
         let id = query.get('id');
         let token = query.get('token');
 
-
         showChangePasswordPage(id, token)
             .then(response => {
-
                 console.log("Token je: " + response.accessToken);
                 localStorage.setItem(ACCESS_TOKEN, response.accessToken);
 
                 const savePasswordRequest = {
-                    password: this.state.password1.value
+                    password: password1.value
                 };
 
                 savePassword(savePasswordRequest)
@@ -62,7 +48,7 @@ class ChangePassword extends Component {
                             message: 'Polling App',
                             description: "Thank you! You have successfully updated your password!",
                         });
-                        this.props.history.push("/login");
+                        history.push("/login");
                     }).catch(error => {
                     notification.error({
                         message: 'Polling App',
@@ -70,121 +56,114 @@ class ChangePassword extends Component {
                     });
                 });
             }).catch(error => {
+            if (error.message.includes("Please send one more password recovery.")) {
+                history.push("/login");
+                notification.error({
+                    message: 'Polling App',
+                    description: error.message || 'Sorry! Something went wrong. Please try again!'
+                });
+            }
+        });
+    };
 
-                if( error.message.includes("Please send one more password recovery.")  ) {
+    const isFormInvalid = () => {
+        return !(password1.validateStatus === 'success' && password2.validateStatus === 'success');
+    };
 
-                    this.props.history.push("/login");
-                    notification.error({
-                        message: 'Polling App',
-                        description: error.message || 'Sorry! Something went wrong. Please try again!'
-                    });
-                }
-            });
-    }
+    const validatePassword1 = (password) => {
+        if (password.length < PASSWORD_MIN_LENGTH) {
+            return {
+                validateStatus: 'error',
+                errorMsg: `Password is too short (Minimum ${PASSWORD_MIN_LENGTH} characters needed.)`
+            };
+        } else if (password.length > PASSWORD_MAX_LENGTH) {
+            return {
+                validateStatus: 'error',
+                errorMsg: `Password is too long (Maximum ${PASSWORD_MAX_LENGTH} characters allowed.)`
+            };
+        } else {
+            return {
+                validateStatus: 'success',
+                errorMsg: null
+            };
+        }
+    };
 
-    isFormInvalid() {
-        return !(  this.state.password1.validateStatus === 'success'
-                 && this.state.password2.validateStatus === 'success' );
-    }
+    const validatePassword2 = (password) => {
+        if (password.length < PASSWORD_MIN_LENGTH) {
+            return {
+                validateStatus: 'error',
+                errorMsg: `Password is too short (Minimum ${PASSWORD_MIN_LENGTH} characters needed.)`
+            };
+        } else if (password.length > PASSWORD_MAX_LENGTH) {
+            return {
+                validateStatus: 'error',
+                errorMsg: `Password is too long (Maximum ${PASSWORD_MAX_LENGTH} characters allowed.)`
+            };
+        } else if (password !== password1.value) {
+            return {
+                validateStatus: 'error',
+                errorMsg: `Passwords do not match ${PASSWORD_DOES_NOT_MATCH}.`
+            };
+        } else {
+            return {
+                validateStatus: 'success',
+                errorMsg: null
+            };
+        }
+    };
 
-    componentWillMount() {
-
-        localStorage.setItem(ACCESS_TOKEN, "");
-    }
-
-    render() {
-
-        return (
-            <div className="signup-container">
-                <h1 className="page-title">Change password</h1>
-                <div className="signup-content">
-                    <Form onSubmit={this.handleSubmit} className="signup-form">
-                        <FormItem
-                            label="New password between 6 to 20 characters"
-                            hasFeedback
-                            validateStatus={this.state.password1.validateStatus}
-                            help={this.state.password1.errorMsg}>
-                            <Input
-                                size="large"
-                                name="password1"
-                                type="password"
-                                autoComplete="off"
-                                placeholder="New password between 6 to 20 characters"
-                                value={this.state.password1.value}
-                                onChange={(event) => this.handleInputChange(event, this.validatePassword1)}/>
-                        </FormItem>
-                        <FormItem
-                            label="Confirm password"
-                            hasFeedback
-                            validateStatus={this.state.password2.validateStatus}
-                            help={this.state.password2.errorMsg}>
-                            <Input
-                                size="large"
-                                name="password2"
-                                type="password"
-                                autoComplete="off"
-                                placeholder="Confirm password"
-                                value={this.state.password2.value}
-                                onChange={(event) => this.handleInputChange(event, this.validatePassword2)}/>
-                        </FormItem>
-                        <FormItem>
-                            <Button type="primary"
-                                    htmlType="submit"
-                                    size="large"
-                                    className="signup-form-button"
-                                    disabled={this.isFormInvalid()} >Update password</Button>
-                            Already registed? <Link to="/login">Login now!</Link>
-                        </FormItem>
-                    </Form>
-                </div>
+    return (
+        <div className="signup-container">
+            <h1 className="page-title">Change password</h1>
+            <div className="signup-content">
+                <Form onSubmitCapture={handleSubmit} className="signup-form">
+                    <FormItem
+                        label="New password between 6 to 20 characters"
+                        hasFeedback
+                        validateStatus={password1.validateStatus}
+                        help={password1.errorMsg}>
+                        <Input
+                            size="large"
+                            name="password1"
+                            type="password"
+                            autoComplete="off"
+                            placeholder="New password between 6 to 20 characters"
+                            value={password1.value}
+                            onChange={(event) => handleInputChange(event, validatePassword1)}
+                        />
+                    </FormItem>
+                    <FormItem
+                        label="Confirm password"
+                        hasFeedback
+                        validateStatus={password2.validateStatus}
+                        help={password2.errorMsg}>
+                        <Input
+                            size="large"
+                            name="password2"
+                            type="password"
+                            autoComplete="off"
+                            placeholder="Confirm password"
+                            value={password2.value}
+                            onChange={(event) => handleInputChange(event, validatePassword2)}
+                        />
+                    </FormItem>
+                    <FormItem>
+                        <Button
+                            type="primary"
+                            htmlType="submit"
+                            size="large"
+                            className="signup-form-button"
+                            disabled={isFormInvalid()}
+                        >
+                            Update password
+                        </Button>
+                        Already registered? <Link to="/login">Login now!</Link>
+                    </FormItem>
+                </Form>
             </div>
-        );
-    }
-
-    validatePassword1 = (password) => {
-        if (password.length < PASSWORD_MIN_LENGTH) {
-            return {
-                validateStatus: 'error',
-                errorMsg: `Password is too short (Minimum ${PASSWORD_MIN_LENGTH} characters needed.)`
-            }
-        } else if (password.length > PASSWORD_MAX_LENGTH) {
-            return {
-                validationStatus: 'error',
-                errorMsg: `Password is too long (Maximum ${PASSWORD_MAX_LENGTH} characters allowed.)`
-            }
-        } else {
-            return {
-                validateStatus: 'success',
-                errorMsg: null,
-            };
-        }
-    }
-
-    validatePassword2 = (password) => {
-        if (password.length < PASSWORD_MIN_LENGTH) {
-            return {
-                validateStatus: 'error',
-                errorMsg: `Password is too short (Minimum ${PASSWORD_MIN_LENGTH} characters needed.)`
-            }
-        } else if (password.length > PASSWORD_MAX_LENGTH) {
-            return {
-                validationStatus: 'error',
-                errorMsg: `Password is too long (Maximum ${PASSWORD_MAX_LENGTH} characters allowed.)`
-            }
-        } else if (password != this.state.password1.value ) {
-            return {
-                validationStatus: 'error',
-                errorMsg: `Passwords does not matches ${PASSWORD_DOES_NOT_MATCH}.`
-            }
-        } else {
-            return {
-                validateStatus: 'success',
-                errorMsg: null,
-            };
-        }
-    }
-
-
-}
+        </div>
+    );
+};
 
 export default ChangePassword;
